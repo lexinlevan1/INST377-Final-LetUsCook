a15ec97e58f2034a5f095dac13bd84bfbe86fccc
loakes1 <loakes1@terpmail.umd.edu>
2024-12-14 20:27:46 -0500

    updated recipe of day

--- public/script.js
+++ public/script.js
@@ -47,16 +47,16 @@ async function getRecipeOfDay(){
     return;
   }
 
-  //const api = await fetch("https://api.spoonacular.com/recipes/complexSearch?sort=random&number=1&apiKey=33d614d7ec7f49f3abc66a103823353f")
-  //  .then((res)=> res.json())
+  const api = await fetch("https://api.spoonacular.com/recipes/complexSearch?sort=random&number=1&apiKey=33d614d7ec7f49f3abc66a103823353f")
+    .then((res)=> res.json())
     const results = api['results']
     console.log("results", results)
     const id = results[0].id
     const recipe_name = results[0].title
     const recipe_image = results[0].image;
 
-   // const ingredients = await fetch(`https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?&apiKey=33d614d7ec7f49f3abc66a103823353f`)
-   // .then((res)=>res.json())
+    const ingredients = await fetch(`https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?&apiKey=33d614d7ec7f49f3abc66a103823353f`)
+    .then((res)=>res.json())
 
     console.log(ingredients)
     const recipeData = {id, recipe_name, recipe_image, ingredients}
@@ -74,7 +74,6 @@ async function displayRecipe(recipe) {
   const recipe_image = dict.recipe_image
   const ingredients = dict.ingredients
   console.log(ingredients)
-  //const {recipe_id, recipe_name, recipe_image, ingredients} = recipe;
 
   document.getElementById('rod_name').innerHTML = `Today's Recipe is: ${recipe_name}`
 
